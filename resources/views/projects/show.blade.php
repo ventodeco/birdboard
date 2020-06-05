5f8956593bf24fe810f61859a50074fd297cfdb6
ventodeco <ventodecoboagoes@gmail.com>
2020-06-05 16:56:46 +0700

    rendering activity with polymorphism

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -66,9 +66,11 @@
                     @endif
                 </div>
             </div>
-            <div class="lg:w-1/4 px-3">
-                @include('projects.card')
+            <div class="lg:w-1/4 px-3 lg:py-8">
+                @include ('projects.card')
+                @include ('projects.activity.card')
             </div>
+
         </div>
     </main>
 
