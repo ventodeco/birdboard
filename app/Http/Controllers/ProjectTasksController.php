bdb272b8e003aa052aed2c7a4e32a81c7a03c4a8
ventodeco <ventodecoboagoes@gmail.com>
2020-06-05 11:17:41 +0700

    project activity feeds part 2

--- app/Http/Controllers/ProjectTasksController.php
+++ app/Http/Controllers/ProjectTasksController.php
@@ -19,16 +19,18 @@ class ProjectTasksController extends Controller
         return redirect($project->path());
     }
 
-    public function update(Project $project, Task $task)    
+    public function update(Project $project, Task $task)
     {
         $this->authorize('update', $task->project);
 
         request()->validate(['body' => 'required']);
-        
-        $task->update([
-            'body' => request('body'),
-            'completed' => request()->has('completed')
-        ]);
+
+        $task->update(['body' => request('body')]);
+
+        if (request()->has('completed')) {
+            $task->complete();
+        }
+
 
         return redirect($project->path());
     }
--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -9,6 +9,7 @@ use Faker\Generator as Faker;
 $factory->define(Task::class, function (Faker $faker) {
     return [
         'body' => $faker->sentence,
-        'project_id' => factory(Project::class)->create()->id
+        'project_id' => factory(Project::class)->create()->id,
+        'completed' => false
     ];
 });
