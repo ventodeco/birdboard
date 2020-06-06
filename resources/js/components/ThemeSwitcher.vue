2bf6a100f284066ecd4084b8c42fed739c3a13e8
ventodeco <ventodecoboagoes@gmail.com>
2020-06-06 16:51:16 +0700

    try with vue

---
+++ resources/js/components/ThemeSwitcher.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="mr-8 flex items-center">
+        <button 
+            class="rounded-full w-4 h-4 border border-gray-900 mr-2 focus:outline-none" 
+            style="background: #fff" 
+            @click="selectedTheme = theme-light"></button>
+        <button 
+            class="rounded-full w-4 h-4 border border-gray-900 mr-2 focus:outline-none" 
+            style="background: #222" 
+            @click="selectedTheme = theme-dark"></button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            returnselectedTheme: 'theme-light'                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+        },
+
+        watch: {
+            selectedTheme(){
+                document.body.className = document.body.className.replace(/theme-\w+/, this.selectedTheme);
+            }
+        }
+    }
+</script>
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -18,4 +18,39 @@
             @endforelse
         </main>
 
+        <modal name="hello-world" classes="p-10 bg-white rounded-lg" height="auto">
+            <h1 class="font-normal mb-16 text-center text-2xl">Let's start something new</h1>
+
+            <div class="flex">
+                <div class="flex-1 mr-4">
+                    <div class="mb-4">
+                        <label for="title" class="text-sm block mb-2">Title</label>
+                        <input type="text" name="title" class="border border-collapse py-2 px-2 text-xs block w-full">
+
+                    </div>
+
+                    <div class="mb-4">
+                        <label for="description" class="text-sm block mb-2">Description</label>
+                        <textarea name="description" class="border border-collapse py-2 px-2 text-xs block w-full" rows="7"></textarea>
+
+                    </div>
+                </div>
+                
+                
+                <div class="flex-1 ml-4">
+                    <div class="mb-4">
+                        <label class="text-sm block mb-2">Need Tasks?</label>
+                        <input type="text" name="title" class="border border-collapse py-2 px-2 text-xs block w-full">
+
+                    </div>
+
+                    <button class="rounded-full border-black"></button>
+                </div>
+
+            </div>
+        </modal>
+
+
+        <a href="" @click.prevent="$modal.show('hello-world')">Show more</a>
+
 @endsection
